8f6835cd5221517864481c986c6d1ed561378b76
bob anzlovar <rcanzlovar@gmail.com>
2018-05-08 01:20:21 -0600

    updated late breaking things to work on another machine

--- backg/js/backgammon_main.js
+++ backg/js/backgammon_main.js
@@ -5,10 +5,13 @@ var black_piece = "<img src='img/bp.gif' alt='X'>";
 var white_piece = "<img src='img/wp.gif' alt='X'>";
 var br = "<br />";
 
+var myBoard; 
+
+
 var boards = {};
 
 // traditional backgammon
-boards['backgammon']  =   { 'long':'Trad Backgammon',
+boards['backgammon']  =   { 'long':'Trad. Backgammon',
 'url':' http://www.bkgm.com/rules.html',
 "01":-2, "02":0, "03":0, "04":0, "05":0, "06":5, 
 "07":0, "08":3, "09":0, "10":0, "11":0, "12":-5,
@@ -21,8 +24,8 @@ boards['backgammon']  =   { 'long':'Trad Backgammon',
 // -5  0  0  0 +3  0 +5  0  0  0  0 -2
 // 12 11 10 09 08 07 06 05 04 03 02 01 
 
-boards['longgammon'] =   {  'long':'Trad Backgammon',
-'url':'  http://www.bkgm.com/variants/LongGammon.html',
+boards['longgammon'] =   {  'long':'LongGammon',
+'url':'http://www.bkgm.com/variants/LongGammon.html',
 "01":-15, "02":0, "03":0, "04":0, "05":0, "06":0, 
 "07":0, "08":0, "09":0, "10":0, "11":0, "12":0,
 "13":0, "14":0, "15":0,"16":0, "17":0, "18":0,
@@ -35,7 +38,7 @@ boards['longgammon'] =   {  'long':'Trad Backgammon',
 // 12 11 10 09 08 07 06 05 04 03 02 01 
 
 boards['dutchgammon'] =   { 'kitty':15, 'long':'Dutch Backgammon',
-'url':' http',
+'url':'http://www.bkgm.com/variants/DutchBackgammon.html',
 "01":0, "02":0, "03":0, "04":0, "05":0, "06":0, 
 "07":0, "08":0, "09":0, "10":0, "11":0, "12":0,
 "13":0, "14":0, "15":0,"16":0, "17":0, "18":0,
@@ -47,8 +50,8 @@ boards['dutchgammon'] =   { 'kitty':15, 'long':'Dutch Backgammon',
 //  0  0  0  0  0  0  0  0  0 -1 -1 -1
 // 12 11 10 09 08 07 06 05 04 03 02 01 
 
-boards['hypergammon'] =   {  'long':'HyperBackgammon',
-'url':' http://www.bkgm.com/faq/Variants.html#what_is_hyper_backgammon',
+boards['hypergammon'] =   {  'long':'Hyper-backgammon',
+'url':' http://www.bkgm.com/variants/HyperBackgammon.html',
 "01":-1, "02":-1, "03":-1, "04":0, "05":0, "06":0, 
 "07":0, "08":0, "09":0, "10":0, "11":0, "12":0,
 "13":0, "14":0, "15":0,"16":0, "17":0, "18":0,
@@ -60,7 +63,7 @@ boards['hypergammon'] =   {  'long':'HyperBackgammon',
 //  0  0  0  0  0  0  0  0  0 -1 -1 -1
 // 12 11 10 09 08 07 06 05 04 03 02 01 
 
-boards['nackgammon'] =   {  'long':'NackGammon',
+boards['nackgammon'] =   {  'long':'Nackgammon',
 'url':'http://www.bkgm.com/variants/Nackgammon.html',
 "01":-2, "02":-2, "03":0, "04":0, "05":0, "06":4, 
 "07":0, "08":3, "09":0, "10":0, "11":0, "12":-4,
@@ -120,13 +123,17 @@ var makepieces = function (e)
 	return returnString;
 }
 
+
 //###################################################
 var setboard = function (e) {
+    console.log('in setboard, e=' + e);
 	var backgammonBoard;
 	console.log('typeof e' + typeof e);
 	if ( typeof e == 'array' && typeof e['10'] == 'number') {
 		backgammonBoard = e;
 //	if ( typeof e == 'object' && typeof e. == 'number') {
+    } else if ( typeof e == 'object' && typeof e['10'] == 'number') {
+        backgammonBoard = e;
 	} else if (typeof e == 'string') {
         document.getElementById("game").innerHTML = e;    
 		backgammonBoard = boards[e];
@@ -140,38 +147,41 @@ var setboard = function (e) {
     for (var key  in backgammonBoard) {
 //    	console.log('value = '+ backgammonBoard[key]);
 //    	console.log('key=' + key);
-    	if (key == 'kitty') {
+
+    	//Kitty or reserve is where unplayed pieces start like in acey deucy or Dutch bacmgammon
+
+        if (key == 'kitty') {
 	        document.getElementById("p1kitty").innerHTML = 
 	        	makepieces({'value': -1 * backgammonBoard["kitty"]});    
 //	        	makepieces({'value': -1 * backgammonBoard["kitty"],'wide':1 });    
 	        document.getElementById("p2kitty").innerHTML = 
 	        	makepieces({'value': backgammonBoard["kitty"]});    
-//	        	makepieces({'value': backgammonBoard["kitty"],'wide':1 });    
-//    	} else if (// backgammonBoard.key == null  ||
-//             backgammonBoard.key == undefined)  {
-//            console.log('key without a home' + key);
-//            document.getElementById(key).innerHTML  
         } else if (key == null ) {
-    		console.log('null key?');
+    		console.log('ERROR null key? does this even happen?');
     	} else {
-    		console.log('2key=' + key);
+            // try to set the board pieces, and if there isn't a matching HTML element, print a warning
+//    		console.log('2key=' + key);
             try {
                    document.getElementById(key).innerHTML = makepieces({'value': backgammonBoard[key] });    
             }
             catch(err) {
-                console.log(key + ": " + err.message);
-            //     document.getElementById("demo").innerHTML = err.message;
+                console.log("WARN: " + key + " " + err.message);
             }
         }
 
     }
-        try {
-                    document.getElementById("p1status").innerHTML = backgammonBoard["long"];
-            }
-            catch(err) {
-                console.log(key + ": " + err.message);
-            //     document.getElementById("demo").innerHTML = err.message;
-            }//    document.getElementById("p1status").innerHTML = document.getElementById("game").innerHTML;
+    try {
+        document.getElementById("p2status").innerHTML = "<a href='" + backgammonBoard["url"] + "' target='_blank'>About</a>";
+    }
+    catch(err) {
+        console.log(key + ": " + err.message);
+    }
+    try {
+        document.getElementById("p1status").innerHTML = backgammonBoard["long"];
+    }
+    catch(err) {
+        console.log(key + ": " + err.message);
+    }
 }
 
 // set up the board here. We can rotate the board and flip it by using a different one. 
@@ -376,15 +386,114 @@ function layboard(arrayin) {
 	setboard(game ? game : 'backgammon');
 }
 
+//#############################################
+function move(e) {
+
+    console.log(e);
+    console.log('typeof e=' + typeof e);
+    console.log('typeof e.from=' + typeof e['from']);
+    console.log('typeof e.to=' + typeof e['to']);
+
+    var from,to,board;
+
+    if ( typeof e == 'object' && typeof e['from'] == 'string') {
+        from = e['from'];
+        to = e['to'];
+    }
+
+    console.log('move from/to' + from + '/' + to);
+    // here  we process what is there and can we even do this.. 
+    // should clear the from and to before calling this so i don't have to 
+    if (e.board[from] == 0) {
+        alert ("nothing to move");
+        return(e.board); // and bail..
+    } 
+
+    var piecevalue = 1;
+    if (e.board.from < 0) {
+        piecevalue = -1;
+    }
+    alert('piecevalue =' + piecevalue + ' to=' + e.board.to);
+
+
+
+
+    e.board[from] -= piecevalue;
+    e.board[to] += piecevalue;
+
+    console.log('board.from=' + e.board[from] 
+        + 'board.to=' + e.board[to]);
+
+    return e.board;
+}
+//#############################################
 layboard('board1');
+//var backgammonBoddard = boards['hypergammon'];
 //document.getElementById("game").innerHTML = "backgammon";
+myBoard = boards['backgammon'];
+setboard(myBoard);
+console.log(myBoard);
 
-var backgammonBoard = boards['hypergammon'];
-  document.getElementById('01').onclick = function() {
-    console.log(this.id);
+alert('before=' + myBoard['06']);
+myBoard = move({'from':'08','to':'05','board':myBoard});
+myBoard = move({'from':'06','to':'05','board':myBoard});
+//var newboard = move({'from':'06','to':'11','board':myBoard});
+//alert('after=' + myBoard['06']);
+setboard(myBoard);
+
+  var moveprocessor = function() {
     console.log(this);
+    console.log(this.innerHTML);
+    console.log("background = " + this.background);
+    console.log("align = " + this.align);
+    console.log("background = " + this.background);
+    console.log("from is " + 
+        document.getElementById("from").innerHTML);
+    if (document.getElementById("from").innerHTML == "") {
+        document.getElementById("from").innerHTML = this.id;
+    } else if (document.getElementById("to").innerHTML == "") {
+        document.getElementById("to").innerHTML = this.id;
+    }
+    if ( document.getElementById("from").innerHTML != '' 
+        && document.getElementById("to").innerHTML != '' ) {
 
-    alert(this.value);
+        myBoard= move({
+            'from':document.getElementById("from").innerHTML,
+            'to':document.getElementById("to").innerHTML,
+            'board':myBoard
+        });
+        document.getElementById("from").innerHTML = '';
+        document.getElementById("to").innerHTML = '';
 
 
+
+    }
   }
+  document.getElementById('01').onclick =moveprocessor;
+  document.getElementById('02').onclick =moveprocessor;
+  document.getElementById('03').onclick =moveprocessor;
+  document.getElementById('04').onclick =moveprocessor;
+  document.getElementById('05').onclick =moveprocessor;
+  document.getElementById('06').onclick =moveprocessor;
+  document.getElementById('07').onclick =moveprocessor;
+  document.getElementById('08').onclick =moveprocessor;
+  document.getElementById('09').onclick =moveprocessor;
+  document.getElementById('10').onclick =moveprocessor;
+  document.getElementById('11').onclick =moveprocessor;
+  document.getElementById('12').onclick =moveprocessor;
+  document.getElementById('13').onclick =moveprocessor;
+  document.getElementById('14').onclick =moveprocessor;
+  document.getElementById('15').onclick =moveprocessor;
+  document.getElementById('16').onclick =moveprocessor;
+  document.getElementById('17').onclick =moveprocessor;
+  document.getElementById('18').onclick =moveprocessor;
+  document.getElementById('19').onclick =moveprocessor;
+  document.getElementById('20').onclick =moveprocessor;
+  document.getElementById('21').onclick =moveprocessor;
+  document.getElementById('22').onclick =moveprocessor;
+  document.getElementById('23').onclick =moveprocessor;
+  document.getElementById('24').onclick =moveprocessor;
+
+
+
+
